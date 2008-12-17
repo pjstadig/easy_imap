96725674f0d52e773904e39c213425a11d1e1840
Paul Stadig <paul@stadig.name>
2008-12-17 11:36:48 -0500

    Added documentation of the options for Server class

--- lib/easy_imap/server.rb
+++ lib/easy_imap/server.rb
@@ -5,10 +5,22 @@ module EasyIMAP
   class Server
     # Makes a connection to +host+ and returns an instance of Server.
     #
+    # It is recommended that you use this method to connect instead 
+    # of Server#new, because at some time in the future there may be
+    # multiple server classes to smooth over "quirks" in IMAP 
+    # implementations.  This method should automatically select the
+    # right server class for you.
+    #
     # If given a block it will yield the Server instance to the
     # block, and then close the connection to the IMAP server
     # after the block has finished. Otherwise you must close the connection
     # yourself.
+    #
+    # Valid options are:
+    # port:: the server port on which to connect (defaults to 143)
+    # username:: the username with which to connect
+    # password:: the password with which to connect
+    # auth_type:: the authentication type with which to connect (defaults to LOGIN)
     def self.connect(host, options = nil)
       conn = Server.new(host, options)
       if block_given?
@@ -23,8 +35,8 @@ module EasyIMAP
     def initialize(host, options = nil)
       options ||= {}
       @host = host
-      @conn = Net::IMAP.new(host)#, options[:port])
-      if options[:auth_type].downcase == "plain"
+      @conn = Net::IMAP.new(host, options[:port] || 143)
+      if options[:auth_type].to_s.downcase == "plain"
         @conn.login(options[:username], options[:password])
       else
         @conn.authenticate(options[:auth_type] || "LOGIN", options[:username], options[:password])
